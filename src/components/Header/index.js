e48d2e34d3cb049747389deba96d023bb099dfbe
Amanda Anderson <aanderson_120@yahoo.com>
2020-11-18 16:17:07 -0600

    added header and main

---
+++ src/components/Header/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Header.css";
+
+function Header () {
+    return (
+        <div className="header">
+            <h1>Team Directory</h1>
+            <p>Click on each heading to filter or use the search box to narrow your results</p>
+        </div>
+    )
+}
+
+export default Header;
+
+
